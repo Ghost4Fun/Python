54c9e4e76cd4c2bc2024bfc547efb6d3c49258b8
Mariusz <mario.lejkowski@gmail.com>
2019-12-15 20:19:50 +0100

    First task

--- scripts/task1.py
+++ scripts/task1.py
@@ -1,2 +1,11 @@
 import datetime
-now = 
+
+date =  datetime.datetime.now()
+
+name =input("Jak sie nazywasz?: ")
+age = int(input("Ile masz lat?: "))
+
+year = int(100 - age)
+year2 = str(date.year + year)
+
+print(name + " w roku " + year2 + " bedziesz w wieku 100 lat")
